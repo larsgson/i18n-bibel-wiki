2800c2476bd399331fdd39ae12b4e4020e8640e1
larsgson <4819617+larsgson@users.noreply.github.com>
2025-02-18 18:17:41 +0100

    automatic translation es

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import {enText} from "./constants/en"
 import {deText} from "./constants/de"
+import {frText} from "./constants/fr"
+import {esText} from "./constants/es"
+import {ptBRText} from "./constants/pt_BR"
+import {roText} from "./constants/ro"
 
 export {
   enText,
-  deText
+  deText,
+  frText,
+  esText,
+  ptBRText,
+  roText  
 }
